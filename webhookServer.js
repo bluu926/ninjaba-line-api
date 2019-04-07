152289ea0d48758b068019c9c8469eae75305057
Ben Luu <ben@Bens-MacBook-Pro.local>
2019-04-06 22:58:25 -0700

    fix imports

--- webhookServer.js
+++ webhookServer.js
@@ -1,52 +1,51 @@
-'use strict'
-const line = require('./api/lineApi')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+"use strict";
+const line = require("./services/lineService");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 const port = process.env.PORT || 4000;
 
 // need raw buffer for signature validation
-app.use(bodyParser.json({
-  verify (req, res, buf) {
-    req.rawBody = buf
-  }
-}))
+app.use(
+  bodyParser.json({
+    verify(req, res, buf) {
+      req.rawBody = buf;
+    }
+  })
+);
 
 // init with auth
 line.init({
   accessToken: process.env.LINE_NINJA_ACCESS_TOKEN,
   // (Optional) for webhook signature validation
   channelSecret: process.env.LINE_NINJA_CHANNEL_SECRET
-})
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
 });
 
-app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
-// app.post('/webhook/', (req, res, next) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/webhook/", line.validator.validateSignature(), (req, res, next) => {
+  // app.post('/webhook/', (req, res, next) => {
   // get content from request body
   const promises = req.body.events.map(event => {
     // reply message
     console.log(event);
-    return line.client
-      .replyMessage({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: 'text',
-            // text: event.message.text
-            text: 'TEXT'
-          }
-        ]
-      })
-  })
-  Promise
-    .all(promises)
-    .then(() => res.json({success: true}))
-})
+    return line.client.replyMessage({
+      replyToken: event.replyToken,
+      messages: [
+        {
+          type: "text",
+          // text: event.message.text
+          text: "TEXT"
+        }
+      ]
+    });
+  });
+  Promise.all(promises).then(() => res.json({ success: true }));
+});
 
 app.listen(port, () => {
-  console.log('Example app listening on port', port);
-})
+  console.log("Example app listening on port", port);
+});
